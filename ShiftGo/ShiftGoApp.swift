9a552430c0b6493f7c36ba06526e918bd284da2b
doris <doris070714@gmail.com>
2025-08-25 18:09:38 +0800

    Initial Commit

---
+++ ShiftGo/ShiftGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShiftGoApp.swift
+//  ShiftGo
+//
+//  Created by Doris Wen on 2025/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ShiftGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
