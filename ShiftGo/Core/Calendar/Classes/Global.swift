92b928d453d4e47519629a7d947ab0baf06f4461
Doris <doris070714@gmail.com>
2025-08-26 13:40:58 +0800

    add: calendar controller

---
+++ ShiftGo/Core/Calendar/Classes/Global.swift
@@ -0,0 +1,14 @@
+//
+//  Global.swift
+//  ShiftGo
+//
+//  Created by Doris Wen on 2025/8/25.
+//
+
+import Foundation
+import SwiftUI
+
+class Global {
+    static let MAX_PAGE = 100
+    static let CENTER_PAGE = 100 / 2
+}
